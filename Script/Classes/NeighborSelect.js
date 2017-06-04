fc34a5678aad60fbe6158f42c9a5689cec937d84
Guilherme Rossato <guilhermrossato@gmail.com>
2017-06-04 01:48:15 -0300

    Neighbor Selector Class :lock:
    A new class was added to handle selection of neighbor tiles.
    
    When a tile is set to a value, all neighbors should have their
    probabilities updated based on their "new" surroundings: the radius in
    which this happens shouldn't be a hardcoded constant.
    
    A large radius implies that the selected model has structures that
    depend on neighbors that are far away. This variable must be carefully
    because it will drastically change the performance of the world
    generator.

---
+++ Script/Classes/NeighborSelect.js
@@ -0,0 +1,35 @@
+function NeighborSelect(width, height) {
+	let sides = [{
+		name: "top",
+		offset: [0, -1]
+	}, {
+		name: "left",
+		offset: [-1, 0]
+	}, {
+		name: "right",
+		offset: [1, 0]
+	}, {
+		name: "bottom",
+		offset: [0, 1]
+	}];
+	this.select = function(ox, oy, radius = 1, startArray = []) {
+		function rec(x, y) {
+			if (x < 0 || y < 0 || x >= width || y >= height)
+				return
+			let dist = Math.abs(x-ox)+Math.abs(y-oy);
+			let inArray = startArray.some(vec => (vec.x === x && vec.y === y));
+			if ((dist <= radius) && (!inArray)) {
+				startArray.push({x:x, y:y});
+				sides.forEach(side => {
+					rec(x + side.offset[0], y + side.offset[1]);
+				});
+			}
+		}
+		rec(ox, oy);
+		return startArray;
+	}
+}
+
+NeighborSelect.prototype = {
+	constructor: NeighborSelect
+}
